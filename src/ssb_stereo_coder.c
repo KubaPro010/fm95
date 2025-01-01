14410e94ce31de3ed6757cd7ebaec61c6fafa804
KubaPro010 <kuba.pro010@gmail.com>
2025-01-01 02:18:24 +0100

    i have no clue

--- src/ssb_stereo_coder.c
+++ src/ssb_stereo_coder.c
@@ -193,7 +193,7 @@ int main() {
 
             mpx[i] = delay_line(&monoDelay, mono) * MONO_VOLUME +
                 pilot * PILOT_VOLUME +
-                (usb-lsb)*STEREO_VOLUME;
+                (lsb+usb)*STEREO_VOLUME;
         }
 
         if (pa_simple_write(output_device, mpx, sizeof(mpx), NULL) < 0) {
