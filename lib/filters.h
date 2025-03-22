24a2b02fd7ddbf01bc467e6fbcdd6ce1b6e705e7
KubaPro010 <kuba.pro010@gmail.com>
2025-03-22 21:13:15 +0100

    i give up

--- lib/filters.h
+++ lib/filters.h
@@ -15,20 +15,3 @@ void init_preemphasis(ResistorCapacitor *filter, float tau, float sample_rate);
 float apply_preemphasis(ResistorCapacitor *filter, float sample);
 
 float hard_clip(float sample, float threshold);
-
-typedef struct {
-    float phase_error;
-    float loop_filter;
-    float vco_phase;
-    float vco_frequency;
-
-    float reference_frequency;
-    float target_frequency;
-    float multiplier;
-    float sample_rate;
-    float loop_gain;
-    float filter_coefficient;
-} PLL;
-
-void init_pll(PLL *pll, float reference_frequency, float target_frequency, float sample_rate);
-float update_pll(PLL *pll, float reference_signal);
