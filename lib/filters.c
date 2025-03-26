eca15c9544ba582844945d7d5c8c572076a74c71
KubaPro010 <kuba.pro010@gmail.com>
2025-03-26 19:28:38 +0100

    diffrent preemp gain

--- lib/filters.c
+++ lib/filters.c
@@ -3,7 +3,7 @@
 void init_preemphasis(ResistorCapacitor *filter, float tau, float sample_rate) {
 	filter->prev_sample = 0.0f;
 	filter->alpha = expf(-1 / (tau*sample_rate));
-	filter->gain = 1.0f / (1.0f - filter->alpha);
+	filter->gain = 1.0f / sqrtf(1.0f + filter->alpha * filter->alpha);
 }
 float apply_preemphasis(ResistorCapacitor *filter, float sample) {
 	float out = (sample - filter->alpha * filter->prev_sample) * filter->gain;
