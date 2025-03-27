668df44e511c1dd1c73e8d5c4d9b1d1c18d2c1a4
KubaPro010 <kuba.pro010@gmail.com>
2025-03-27 21:42:01 +0100

    what's chatgpt on

--- src/fm95.c
+++ src/fm95.c
@@ -46,16 +46,20 @@
 
 static volatile sig_atomic_t to_run = 1;
 
+#include <arm_neon.h>
+
 void uninterleave(const float *input, float *left, float *right, size_t num_samples) {
 #if USE_NEON
+	size_t num_frames = num_samples / 2;
 	size_t i = 0;
-	for (; i + 4 <= num_samples; i += 4) {
-		float32x4x2_t interleaved = vld2q_f32(input + i * 2);
+
+	for (; i + 4 <= num_frames; i += 4) {
+		float32x4x2_t interleaved = vld2q_f32(input + i * 2 * 2);
 		vst1q_f32(left + i, interleaved.val[0]);
 		vst1q_f32(right + i, interleaved.val[1]);
 	}
 
-	for (; i < num_samples / 2; i++) {
+	for (; i < num_frames; i++) {
 		left[i] = input[i * 2];
 		right[i] = input[i * 2 + 1];
 	}
