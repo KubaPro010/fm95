25c0a3435941a159befbf0d331394e27d0c0443a
KubaPro010 <kuba.pro010@gmail.com>
2025-03-29 22:31:36 +0100

    some safety changes to bs412

--- lib/bs412.c
+++ lib/bs412.c
@@ -5,7 +5,7 @@ float dbr_to_deviation(float dbr) {
 }
 
 float deviation_to_dbr(float deviation) {
-	return 20*log10f(deviation/19000.0f);
+	return 20 * log10f((deviation + 1e-6f) / 19000.0f);
 }
 
 void init_modulation_power_measure(MPXPowerMeasurement* mpx, int sample_rate) {
@@ -14,7 +14,7 @@ void init_modulation_power_measure(MPXPowerMeasurement* mpx, int sample_rate) {
 	mpx->sample_rate = sample_rate;
 }
 
-float measure_mpx(MPXPowerMeasurement* mpx, int deviation) {
+float measure_mpx(MPXPowerMeasurement* mpx, float deviation) {
 	mpx->sample += deviation;
 
 	float avg_deviation = (float)mpx->sample / mpx->i;
--- lib/bs412.h
+++ lib/bs412.h
@@ -12,4 +12,4 @@ float dbr_to_deviation(float dbr);
 float deviation_to_dbr(float deviation);
 
 void init_modulation_power_measure(MPXPowerMeasurement *mpx, int sample_rate);
-float measure_mpx(MPXPowerMeasurement *mpx, int deviation);
+float measure_mpx(MPXPowerMeasurement *mpx, float deviation);
