59a8359909230819fb50f20fe7dca82cd81c3a8f
KubaPro010 <kuba.pro010@gmail.com>
2025-03-29 22:21:44 +0100

    change logic again

--- src/fm95.c
+++ src/fm95.c
@@ -526,9 +526,9 @@ int main(int argc, char **argv) {
 			if(sca_on) output[i] += modulate_fm(&sca_mod, hard_clip(current_sca_in, sca_clipper_threshold))*SCA_VOLUME;
 
 			float mpower = measure_mpx(&power, output[i] * 75000);
-			if (fabsf(mpower) > mpx_power) {
+			if (mpower > mpx_power) {
 				float excess = mpower - mpx_power;
-				float attenuation_db = excess * 0.5f;
+				float attenuation_db = excess * 0.75f;
 				float attenuation_linear = dbr_to_deviation(attenuation_db)/75000;
 				output[i] *= attenuation_linear;
 				printf("Overpower! %f*%f/%f\n", mpower, attenuation_linear, mpx_power);
