9bd229e11ba803ab3c5e0ecd73d65ad6e06654a6
KubaPro010 <kuba.pro010@gmail.com>
2025-04-18 22:48:34 +0200

    fix

--- lib/bs412.c
+++ lib/bs412.c
@@ -15,16 +15,16 @@ void init_modulation_power_measure(MPXPowerMeasurement* mpx, int sample_rate) {
 }
 
 float measure_mpx(MPXPowerMeasurement* mpx, float deviation) {
-    mpx->sample += deviation * deviation; // rmS
-    mpx->i++;
+	mpx->sample += deviation * deviation; // rmS
+	mpx->i++;
 
-    float avg_deviation = sqrtf(mpx->sample / mpx->i); // RMs
-    float modulation_power = deviation_to_dbr(avg_deviation);
+	float avg_deviation = sqrtf(mpx->sample / mpx->i); // RMs
+	float modulation_power = deviation_to_dbr(avg_deviation);
 
-    if (mpx->i >= mpx->sample_rate * 60) {
-        mpx->sample = 0;
-        mpx->i = 0;
-    }
+	if (mpx->i >= mpx->sample_rate * 60) {
+		mpx->sample = 0;
+		mpx->i = 0;
+	}
 
-    return modulation_power;
+	return modulation_power;
 }
