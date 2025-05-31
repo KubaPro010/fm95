4fdb2dff0d84f4f033c6fd8bc4f540b0b6276139
KubaPro010 <kuba.pro010@gmail.com>
2025-05-31 11:51:26 +0200

    bug is 100% related to bs412

--- src/fm95.c
+++ src/fm95.c
@@ -578,13 +578,6 @@ int main(int argc, char **argv) {
 				float target_gain = dbr_to_deviation(-excess_power)/mpx_deviation;
 				bs412_audio_gain = 0.9f * bs412_audio_gain + 0.1f * target_gain;
 				audio *= bs412_audio_gain;
-			} else if (peak_mpower > (mpx_power + 0.1f)) {
-				float excess_power = peak_mpower - mpx_power - 0.1f;
-				excess_power = deviation_to_dbr(dbr_to_deviation(excess_power) - dbr_to_deviation(peak_mpx_only));
-
-				float target_gain = dbr_to_deviation(-excess_power)/mpx_deviation;
-				bs412_audio_gain = 0.8f * bs412_audio_gain + 0.2f * target_gain;
-				audio *= bs412_audio_gain;
 			}
 
 			iirfilt_rrrf_execute(mpx_lpf, audio, &audio); // Should have no effect, as audio should be 0-15, and 23-53, this is a filter for 53, assuming the filter is good, this is precaution and recomendation
