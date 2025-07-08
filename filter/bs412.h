779b8d89b0d0334de217d4baf8a32bdcbf04838d
KubaPro010 <kuba.pro010@gmail.com>
2025-07-08 23:18:51 +0200

    im about to lose my HEADer

--- filter/bs412.h
+++ filter/bs412.h
@@ -25,5 +25,5 @@ typedef struct
 float dbr_to_deviation(float dbr);
 float deviation_to_dbr(float deviation);
 
-void init_bs412(BS412Compressor *mpx, float mpx_deviation, float target_power, float attack, float release, int sample_rate);
+void init_bs412(BS412Compressor *mpx, float mpx_deviation, float target_power, float attack, float release, float max, int sample_rate);
 float bs412_compress(BS412Compressor *mpx, float average);
